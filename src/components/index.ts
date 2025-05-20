73fbbbaa1476fe68515e79e5c916666b47f89383
IsseiSuar <cristsuar@gmail.com>
2025-05-20 10:52:19 -0600

    Add index files for components, enums, interfaces, mocks, navigation, and services

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export * from './accordion/accordion';
+export * from './carusel/carusel.component';
+export * from './header-details/header-details';
+export * from './header/header.component';
+export * from './sections/sections.component';
